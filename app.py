6d6e5a00058619c2681e133481e5e4179fdbf141
Ushan Dilusha <99611000+Ushan-Dilusha@users.noreply.github.com>
2025-02-15 01:10:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ main_app = FastAPI()
 main_app.mount("/quickdraw", quick_draw_app)
 
 if __name__ == "__main__":
-    uvicorn.run("app:main_app", host="0.0.0.0", port=9091, reload=True)
+    uvicorn.run("app:main_app", host="0.0.0.0", port=9090, reload=True)
