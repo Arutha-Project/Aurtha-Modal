4a2a6c9218fd82ba9ac0bb6765649db423f89206
Isharam99 <IT21183904@my.sliit.lk>
2025-05-25 15:17:03 +0530

    prediction_controller.py updated

--- Controller/Letter/prediction_controller.py
+++ Controller/Letter/prediction_controller.py
@@ -3,6 +3,8 @@ from urllib.request import Request
 from fastapi import APIRouter, UploadFile, File
 from fastapi.responses import JSONResponse
 from Services.Letter.prediction_service import predict_letter_sinhala,predict_letter_sinhala_activity
+from Services.Letter.english_prediction_service import predict_letter_english,predict_letter_english_activity
+
 
 router = APIRouter()
 
@@ -18,6 +20,23 @@ async def predict_sinhala_letter_base64(payload: dict):
         return JSONResponse(content={"error": "Missing frame data"}, status_code=400)
 
     result = await predict_letter_sinhala_activity(frame_base64)
+    if isinstance(result, dict) and "error" in result:
+        return JSONResponse(content=result, status_code=400)
+    return JSONResponse(content={"predicted_letter": result})
+
+@router.post("/predict-letter-english")
+async def predict_english_letter(file: UploadFile = File(...)):
+    result = await predict_letter_english(file)
+    return JSONResponse(content={"letter": result})
+
+
+@router.post("/predict-activity/english-letter")
+async def predict_english_letter_base64(payload: dict):
+    frame_base64 = payload.get("frame")
+    if not frame_base64:
+        return JSONResponse(content={"error": "Missing frame data"}, status_code=400)
+
+    result = await predict_letter_english_activity(frame_base64)
     if isinstance(result, dict) and "error" in result:
         return JSONResponse(content=result, status_code=400)
     return JSONResponse(content={"predicted_letter": result})
